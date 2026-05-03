2028a76076745bfb5eb8632e4ee255f1967e6b75
LilDAng31 <angeldeejesusthomasd@gmail.com>
2026-05-02 21:14:35 -0500

    Portafolio

---
+++ assets/script/script.js
@@ -0,0 +1,9 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu-navegation");
+
+menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
+
+
+/// no se por que no sirve :(
